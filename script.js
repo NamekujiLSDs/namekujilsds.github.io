2b1ddf3a9b2bb9d189976f4e562f1736bfc3599c
NamekujiLSDs <81683675+NamekujiLSDs@users.noreply.github.com>
2021-12-07 14:58:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
 // 枠組みの中身を書き換えて云々
-const rCont = document.getElementById('rightContents');
-// ページのタイトルを云々
-const pTitle = document.getElementById('pTitle');
-// シェアボタン云々
-const share = document.getElementById('shareBtn');
-
-// 色テーブル
-const colors = [
-    '#ffaadb',
-    '#aaefff',
-    '#aaffc9',
-    '#efffaa',
-    '#ffadaa',
-    '#e9aaff'
-];
+const rCont = document.getElementById('content');
 
 // 文字列選択の無効化
 document.ondragstart = function () {
     return false;
 };
 
-// ページ読み込み時に実行(色のランダム化)
-window.onload = function () {
-    document.getElementsByTagName('html')[0].style.setProperty('--accent-color', colors[Math.round(Math.random() * (colors.length - 1))])
-}
-
-
-
 // ページ切り替え
 const loadSubPage = (name) => {
     rCont.innerHTML = `<iframe src='${name.toLowerCase()}.html'>`;
-    pTitle.innerHTML = `Namekuji - ${name}`;
     document.getElementsByTagName('html')[0].style.setProperty('--accent-color', colors[Math.round(Math.random() * (colors.length - 1))])
 }
 
@@ -40,3 +18,30 @@ function copyUrl() {
     navigator.clipboard.writeText(url);
     share.innerHTML = "URL Copied";
 }
+// 色テーブル
+const colors = [
+  "#ffaadb",
+  "#aaefff",
+  "#aaffc9",
+  "#efffaa",
+  "#ffadaa",
+  "#e9aaff"
+];
+
+// ページ読み込み時に実行(色のランダム化)
+window.onload = function () {
+  document
+    .getElementsByTagName("html")[0]
+    .style.setProperty(
+      "--accent-color",
+      colors[Math.round(Math.random() * (colors.length - 1))]
+    );
+};
+
+//ナビゲーションバー
+const list = document.querySelectorAll(".list");
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("click", activeLink));
