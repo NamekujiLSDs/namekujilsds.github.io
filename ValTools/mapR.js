fe829382346819fcff2400f5b0cb05817c74a524
Namekujilsds <okosimasu3@gmail.com>
2022-12-10 22:59:00 +0900

    ランダムマップでマップ選択ができるように

--- ValTools/mapR.js
+++ ValTools/mapR.js
@@ -9,43 +9,64 @@ const mapPool = [
     "SPLIT",
 ];
 
-function randomAgent() {
-    let selected = mapPool[Math.floor(Math.random() * mapPool.length + 0)];
-    console.log(selected);
+let mapRandPool = [];
 
-    document
-        .getElementById("mapImg")
-        .setAttribute("src", `img/map_big/${selected}.webp`);
 
-    document.getElementById("mapName").textContent = selected;
-
-    let ad = Math.floor(Math.random() * 2 + 0);
-    if (ad === 0) {
-        document.getElementsByClassName("t1AD")[0].textContent = "ATTACKER";
-        document.getElementsByClassName("t2AD")[0].textContent = "DEFENDER";
-    } else if (ad === 1) {
-        document.getElementsByClassName("t1AD")[0].textContent = "DEFENDER";
-        document.getElementsByClassName("t2AD")[0].textContent = "ATTACKER";
-    }
-}
-
-function startRoll() {
-    document.getElementById("rerollBtn").setAttribute("class", "hidden");
-    document.getElementById("result").setAttribute("class", "visible");
-    document.getElementById("menu").setAttribute("class", "hidden");
-    let count = 0;
-    let roll = Math.floor(Math.random() * 40 + 20);
-    let int = setInterval(function () {
-        randomAgent();
-        count++;
-        console.log(count);
-        if (count === roll) {
-            clearInterval(int);
-            document.getElementById("rerollBtn").setAttribute("class", "visivle");
+function mapBan() {
+    for (let node of document.getElementsByClassName('map')) {
+        if (!node.checked) {
+            node.parentNode.setAttribute("class", "banned");
+            node.parentNode.childNodes[7].setAttribute('class', 'visible');
+        } else if (node.checked) {
+            node.parentNode.removeAttribute("class");
+            node.parentNode.childNodes[7].setAttribute('class', 'hidden');
         }
-    }, 100);
+    }
 }
 
 document.onselectstart = function () {
     return false;
+}
+
+function roll() {
+    document.getElementById("roll").setAttribute("class", "hidden");
+    let count = 0;
+    mapRandPool = [];
+    for (let node of document.getElementsByClassName('map')) {
+        if (node.checked) {
+            mapRandPool.push(mapPool[count])
+        }
+        count++
+    }
+    console.log(mapRandPool); //マッププールが完成
+
+    let intervalCount = 0;
+    let rollVal = Math.floor(Math.random() * 40 + 20);
+    let int = setInterval(function () {
+        randomMap();
+        intervalCount++;
+        console.log(intervalCount)
+        if (intervalCount === rollVal) {
+            clearInterval(int);
+            document.getElementById("roll").setAttribute("class", "visible");
+        }
+    }, 50);
+}
+
+randomMap = () => {
+
+    let ramNum = Math.floor(Math.random() * mapRandPool.length + 0);
+    const mapDisplay = document.getElementById('pickedMapImg')
+    mapDisplay.setAttribute('src', `./img/map_big/${mapRandPool[ramNum]}.webp`);
+    const mapName = document.getElementById('mapName');
+    mapName.textContent = `${mapRandPool[ramNum]}`;
+
+    let ad = Math.floor(Math.random() * 2 + 0);
+    if (ad === 0) {
+        document.getElementById("t1AD").textContent = "ATTACKER";
+        document.getElementById("t2AD").textContent = "DEFENDER";
+    } else if (ad === 1) {
+        document.getElementById("t1AD").textContent = "DEFENDER";
+        document.getElementById("t2AD").textContent = "ATTACKER";
+    }
 }
