5a24594bdf464aa04bbd65d96761be00b86271ac
Namekujilsds <okosimasu3@gmail.com>
2022-12-10 16:53:58 +0900

    いろいろ修正

--- ValTools/index.js
+++ ValTools/index.js
@@ -3,18 +3,18 @@ window.addEventListener("load", load);
 function load() {
     const agent = document.getElementById("agentR");
     let val = Math.floor(Math.random() * 19 + 0);
-    let imgUrl = `img/agent_thumb/agent_${val}.png`;
+    let imgUrl = `./img/agent_thumb/agent_${val}.png`;
     agent.setAttribute("style", `background-image:url(${imgUrl})`);
 
     const map = document.getElementById("mapR");
     let val2 = Math.floor(Math.random() * 7 + 0);
-    let imgUrl2 = `img/map_thumb/map_${val2}.png`;
+    let imgUrl2 = `./img/map_thumb/map_${val2}.png`;
     map.setAttribute("style", `background-image:url(${imgUrl2})`);
 
     let val3 = Math.floor(Math.random() * 10 + 0);
     document.body.setAttribute(
         "style",
-        `background-image: url('img/bg/bg_${val3}.webp');`
+        `background-image: url('./img/bg/bg_${val3}.webp');`
     );
 }
 
